7ac9458fb4118b0fda9eea485e17e2dce4014225
Luke-Antra <h.luke1999@gmail.com>
2019-09-20 15:06:38 +0200

    async ESI client, initial commit of core module

--- esiclient.py
+++ esiclient.py
@@ -1,9 +1,24 @@
 import aiohttp
 import asyncio
 import time
+import json
+from aiocache import Cache
+from aiocache import cached
 
-async def fetchIDs(namelist):    
+with open('chars.json') as f:
+    chartext = json.load(f)
+    f.close()
+
+async def fetch_charData(namelist):
+    # perform the requests for IDs and character data
     async def fetch(session, url, name):
+        data = await fetch_id(session, url, name)
+        char = await fetch_char(session, data['character'][0])
+        return char
+    
+    # make request to /search/ endpoint using character name to get character ID, cached by character name
+    @cached(ttl=1000, cache=Cache.MEMORY, key_builder=keybuild_search)
+    async def fetch_id(session, url, name):
         params = [
             ('categories', 'character'), 
             ('datasource', 'tranquility'), 
@@ -13,7 +28,17 @@ async def fetchIDs(namelist):
 
         async with session.get(url, params=params) as response:
             data = await response.json()
-            return {"name":name, "charID": data['character'][0]}
+            return data
+
+    # make request to /characters/ endpoint using character ID, cached by character ID
+    @cached(ttl=1000, cache=Cache.MEMORY, key_builder=keybuild_id)
+    async def fetch_char(session, charID):
+        params = [
+            ('datasource', 'tranquility'), 
+        ]
+        url = 'https://esi.evetech.net/latest/characters/'
+        async with session.get(url + str(charID), params=params) as response:
+            return await response.json()
 
     async def fetch_all(name_list):
         url = 'https://esi.evetech.net/latest/search/'
@@ -23,31 +48,23 @@ async def fetchIDs(namelist):
 
     return await fetch_all(namelist)
 
-async def fetch_charData(charID_list):    
-    async def fetch(session, charID):
-        params = [
-            ('datasource', 'tranquility'), 
-        ]
-        url = 'https://esi.evetech.net/latest/characters/'
-        async with session.get(url + str(charID), params=params) as response:
-            return await response.json()
-
-    async def fetch_all(charID_list):
-        async with aiohttp.ClientSession() as session:
-            results = await asyncio.gather(*[fetch(session, entry['charID']) for entry in charID_list], return_exceptions=True)
-            return results
-
-    return await fetch_all(charID_list)
-
 async def full_fetch(namelist):
-    ids = await fetchIDs(namelist)
-    data = await fetch_charData(ids)
+    data = await fetch_charData(namelist)
     return data
 
+def keybuild_id(f, session, charID):
+    return charID
+
+def keybuild_search(f, session, url, name):
+    return name
+
 if __name__ == "__main__":
-    names = ['Elena Amarin', 'Ilian Amarin', 'Arden Amarin']
     start = time.time()
-    data = asyncio.run(full_fetch(names))
+    data = asyncio.run(full_fetch(chartext))
     end = time.time()
-    print(data)
     print(end - start)
+    start = time.time()
+    data = asyncio.run(full_fetch(chartext))
+    end = time.time()
+    print(end - start)
+    input()
